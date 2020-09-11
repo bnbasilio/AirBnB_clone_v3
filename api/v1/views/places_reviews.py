80da356b8f1756582598e63d90071924546beed7
Nic Basilio <bnbasilio@gmail.com>
2020-09-11 02:32:09 +0000

    debug review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -7,23 +7,20 @@ from models import storage
 from models.place import Place
 from models.review import Review
 from models.user import User
-app = Flask(__name__)
 
 
 @app_views.route('/api/v1/places/<place_id>/reviews', methods=['GET'],
                  strict_slashes=False)
 def all_reviews(place_id):
     """ retrieves the list of all Review objects of a Place """
-    place = storage.get(Place, place_id)
-
-    if place is None:
+    if not storage.get(Place, place_id):
         abort(404)
 
     all_reviews = []
-    for review in storage.all('Reviews').values():
-        if place_id == review.place_id:
+    for review in storage.all(Review).values():
+        if place_id == review.to_dict()['place_id']:
             all_reviews.append(review.to_dict())
-        return jsonify(all_reviews)
+    return jsonify(all_reviews)
 
 
 @app_views.route('/api/v1/reviews/<review_id>', methods=['GET'],
@@ -31,11 +28,10 @@ def all_reviews(place_id):
 def get_review(review_id):
     """ retrieves a Review object """
     review = storage.get(Review, review_id)
-
-    if review is None:
-        abort(404)
-    else:
+    if review:
         return jsonify(review)
+    else:
+        abort(404)
 
 
 @app_views.route('/api/v1/reviews/<review_id>', methods=['DELETE'],
@@ -43,13 +39,12 @@ def get_review(review_id):
 def delete_review(review_id):
     """ deletes a Review object """
     review = storage.get(Review, review_id)
-
-    if review is None:
-        abort(404)
-    else:
+    if review:
         storage.delete(review)
         storage.save()
         return jsonify({}), 200
+    else:
+        abort(404)
 
 
 @app_views.route('/api/v1/places/<place_id>/reviews', methods=['POST'],
@@ -67,10 +62,10 @@ def create_review(place_id):
         abort(404)
     if 'text' not in review_obj:
         abort(400, {'Missing text'})
-    new_review = Review(**review_obj)
-    storage.new(new_review)
+    this_review = Review(**review_obj)
+    storage.new(this_review)
     storage.save()
-    return jsonify(new_review.to_dict()), 201
+    return jsonify(this_review.to_dict()), 201
 
 
 @app_views.route('/api/v1/reviews/<review_id>', methods=['PUT'],
